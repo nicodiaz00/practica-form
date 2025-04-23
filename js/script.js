5cc244fd627dc23bd3b5f195af7a1407f33e4553
nico <ngdiaz00@gmail.com>
2025-04-23 11:24:30 -0300

    Practica formulario y evento

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-const params = new URLSearchParams(window.location.search);
+const $formulario = document.querySelector('#formulario-registro');
+
+$formulario.addEventListener('submit', function (e)  {
+    e.preventDefault();
+
+    const datos = new FormData($formulario);
+    console.log(`${datos.get('nombre')} ${datos.get('apellido')} ${datos.get('email')} ${datos.get('contrasena')} ${datos.get('genero')} ${datos.get('pais')}`);
+    /*
+    console.log(`Nombre: ${datos.get('nombre')}`);
+    console.log(`Apellido: ${datos.get('apellido')}`);
+    console.log(`Email: ${datos.get('email')}`);
+    console.log(`Contraseña: ${datos.get('contrasena')}`);
+    console.log(`Género: ${datos.get('genero')}`);
+    console.log(`País: ${datos.get('pais')}`);
+    */
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const params = new URLSearchParams(window.location.search);
 
 const nombre = params.get('nombre');
 const apellido = params.get('apellido');
@@ -22,7 +56,7 @@ $titulo.textContent= 'Gracias por registrarte';
 $parrafo.innerHTML = `Nombre: ${nombre}<br> Apellido: ${apellido}<br> Dni: ${dni}<br> Fecha Nacimiento: ${fechaNacimiento}<br> Genero: ${genero}<br> Pais: ${pais} `
 $contenedor.appendChild($titulo);
 $contenedor.appendChild($parrafo)
-
+*/
 
 
 
