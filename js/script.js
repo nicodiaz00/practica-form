f38220b1728ec3367b6bd69bb3e9fb0dbce5e321
nico <ngdiaz00@gmail.com>
2025-04-30 11:03:08 -0300

    agrego registro.html

--- js/script.js
+++ js/script.js
@@ -1,43 +1,10 @@
-const $formulario = document.querySelector('#formulario-registro');
 
-$formulario.addEventListener('submit', function (e)  {
-    e.preventDefault();
-
-    const datos = new FormData($formulario);
-    console.log(`${datos.get('nombre')} ${datos.get('apellido')} ${datos.get('email')} ${datos.get('contrasena')} ${datos.get('genero')} ${datos.get('pais')}`);
-    /*
-    console.log(`Nombre: ${datos.get('nombre')}`);
-    console.log(`Apellido: ${datos.get('apellido')}`);
-    console.log(`Email: ${datos.get('email')}`);
-    console.log(`Contraseña: ${datos.get('contrasena')}`);
-    console.log(`Género: ${datos.get('genero')}`);
-    console.log(`País: ${datos.get('pais')}`);
-    */
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const params = new URLSearchParams(window.location.search);
+const params = new URLSearchParams(window.location.search);
 
 const nombre = params.get('nombre');
 const apellido = params.get('apellido');
-const dni = params.get('dni');
-const fechaNacimiento = params.get('fecha');
+const email = params.get('email');
+const contrasena = params.get('contrasena');
 const genero = params.get('genero');
 const pais = params.get('pais');
 
@@ -45,7 +12,7 @@ const pais = params.get('pais');
 
 
 
-const $contenedor = document.querySelector('.contendor-main');
+const $contenedor = document.querySelector('.contenedor-main');
 const $titulo = document.createElement('h3');
 const $parrafo = document.createElement('p');
 
@@ -53,10 +20,13 @@ const $parrafo = document.createElement('p');
 
 
 $titulo.textContent= 'Gracias por registrarte';
-$parrafo.innerHTML = `Nombre: ${nombre}<br> Apellido: ${apellido}<br> Dni: ${dni}<br> Fecha Nacimiento: ${fechaNacimiento}<br> Genero: ${genero}<br> Pais: ${pais} `
+$parrafo.innerHTML = `Nombre: ${nombre}<br> Apellido: ${apellido}<br> Email: ${email}<br>Contraseña: ${contrasena}<br> Genero: ${genero}<br> Pais: ${pais} `
+
+
 $contenedor.appendChild($titulo);
+
 $contenedor.appendChild($parrafo)
-*/
+
 
 
 
